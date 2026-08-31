3696f9c8b28fce36b7715a976d740bcead983ae7
ehtasham29 <me21chb0b30@student.nitw.ac.in>
2026-08-31 13:16:39 +0530

    logger and exception done

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path : str) -> List[str] :
+    '''
+    This funtion will return the list of requirements.
+    '''
+    requirements = []
+    with open(file_path) as file_obj : 
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n','')for req in requirements]
+
+        if HYPEN_E_DOT in requirements :
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 
 
 setup(
@@ -7,5 +24,5 @@ setup(
     author = 'Ehtasham',
     author_email = 'me21chb0b30@student.nitw.ac.in',
     packages = find_packages(),
-    install_requires = ['pandas','numpy','seaborn'] 
+    install_requires = get_requirements('requirements.txt') 
 )
